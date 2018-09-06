69e11f6d2d6f1b1cd0669fa69b984d6f5dce8f3a
Tim Fischer <emailtimfischer@gmail.com>
2018-09-06 13:52:44 -0600

    Import browser router, Route to app

---
+++ src/containers/DropSubmitForm/DropSubmitForm.test.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Nav from '../Nav'
+
+class DropSubmitForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="drop-submit">
+        <Nav />
+
+      </div>
+    )
+  }
+}
+
+export default DropSubmitForm;
