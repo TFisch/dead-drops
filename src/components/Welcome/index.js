79f4a9d7907e835ec984ddfbbdbea073af3ffcce
Tim Fischer <emailtimfischer@gmail.com>
2018-09-06 12:00:14 -0600

    import welcome to app

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom';
 export const Welcome = () => {
   return (
     <div>
+      <h1>WELCOME</h1>
       <a href='#/' alt="pinterest-login" />
     </div>
   )
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Welcome } from '../../components/Welcome/'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Welcome />
       </div>
     );
   }
