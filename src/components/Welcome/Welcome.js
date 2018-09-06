f64b19cc77d791acfc8c7d73329a60da80cb2896
Tim Fischer <emailtimfischer@gmail.com>
2018-09-06 11:54:19 -0600

    Set link in welcome screen

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './style.css';
+import { Route } from 'react-router-dom';
+
+export const Welcome = () => {
+  return (
+    <div>
+      <a href='#/' alt="pinterest-login" />
+    </div>
+  )
+}
