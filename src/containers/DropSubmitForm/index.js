4b7e5a829fec7fb0c8ee55e46bbd71962c3f8835
Tim Fischer <emailtimfischer@gmail.com>
2018-09-06 15:47:03 -0600

    Add input fields on DropSubmit

--- src/containers/DropSubmitForm/index.js
+++ src/containers/DropSubmitForm/index.js
@@ -7,9 +7,18 @@ export class DropSubmitForm extends Component {
 
     }
   }
+
+
   render() {
     return (
       <div className="drop-submit">
+        <div className="form">
+          <input name="difficulty" placeholder="difficulty" type="text" />
+          <input name="north-coord" placeholder="north coordinates" type="text" />
+          <input name="west-coord" placeholder="west coordinates" type="text" />
+          <input name="code" placeholder="Unique Verification Code" type="text" />
+          <button>SUBMIT</button>
+        </div>
       </div>
     )
   }
