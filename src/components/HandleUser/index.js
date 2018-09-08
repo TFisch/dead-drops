1d0984a8889cbc0622bb88814bf77ea7ca4d98ca
Tim Fischer <emailtimfischer@gmail.com>
2018-09-07 19:05:30 -0600

    Change HandleUser to stateful

--- src/components/HandleUser/index.js
+++ src/components/HandleUser/index.js
@@ -1,39 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { OauthReceiver } from 'react-oauth-flow';
 import { id } from '../../hidden/hidden'
 
 
-const HandleUser = () => {
-  // handleSuccess = async (accessToken, { response, state }) => {
-  //   console.log('Successfully authorized');
-  //   await setProfileFromDropbox(accessToken);
-  //   await redirect(state.from);
-  // };
+class HandleUser extends Component {
+  handleSuccess = async (accessToken, { response, state }) => {
 
-  // handleError = error => {
-  //   console.error('An error occured');
-  //   console.error(error.message);
-  // };
+    console.log('Successfully authorized');
+    // await setProfileFromDropbox(accessToken);
+    // await redirect(state.from);
+  };
 
+  handleError = error => {
+    console.error('An error occured');
+    console.error(error.message);
+  };
 
-  return (
-    <OauthReceiver
-      tokenUrl="https://api.pinterest.com/v1/oauth/token"
-      clientId={'4987807426915878592'}
-      clientSecret={id}
-      redirectUri="https://localhost:3000/"
-      // onAuthSuccess={this.handleSuccess}
-      // onAuthError={this.handleError}
-      render={({ processing, state, error }) => (
-        <div>
-          {processing && <p>Authorizing now...</p>}
-          {error && (
-            <p className="error">An error occured: {error.message}</p>
-          )}
-        </div>
-      )}
-    />
-  );
+  reunder() {
+    return (
+      <OauthReceiver
+        tokenUrl="https://api.pinterest.com/v1/oauth/token"
+        clientId={'4987807426915878592'}
+        clientSecret={id}
+        redirectUri="https://localhost:3000/"
+        onAuthSuccess={this.handleSuccess}
+        onAuthError={this.handleError}
+        render={({ processing, state, error }) => (
+          <div>
+            {processing && <p>Authorizing now...</p>}
+            {error && (
+              <p className="error">An error occured: {error.message}</p>
+            )}
+          </div>
+        )}
+      />
+    )
+  }
 }
 
 
