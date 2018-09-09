a0d659b7521b55af2a798484458adcb37518c2cf
Tim Fischer <emailtimfischer@gmail.com>
2018-09-08 22:36:35 -0600

    Get code from url

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import HandleUser from '../../components/HandleUser';
+import { id } from '../../hidden/hidden'
+import { OauthReceiver, OauthSender } from 'react-oauth-flow';
+import { retrieveToken } from '../../api/apiCalls'
+
 
 export class Profile extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+
+    }
   }
 
+
+  componentDidMount = async () => {
+    const code = this.getUrlVars();
+    await retrieveToken(code);
+  }
+
+  getUrlVars = () => {
+    let vars = "";
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+      vars = value;
+    });
+    return vars;
+  }
+
+
+
+
   render() {
+
     return (
-      <HandleUser />
+      <div>
+        <h1>HI</h1>
+      </div>
     )
   }
 
