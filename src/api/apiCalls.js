9434d1180bbcf1ce52f3502c050039a164fef964
Tim Fischer <emailtimfischer@gmail.com>
2018-09-08 22:35:32 -0600

    Write retrieveToken

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,8 +1,27 @@
+import { id } from '../../hidden/hidden'
 
+// export const fetchUserImage = async => {
+//   const url = `https://api.pinterest.com/v1/users/{user}/?access_token={token}&fields=image`
+//   const response = await fetch(url);
+// }
 
+export const retrieveToken = async (code) => {
+  const data = {
+    grant_type: 'authorization_code',
+    client_id: '4987807426915878592',
+    client_secret: id,
+    code: 'code'
+  }
 
-export const fetchUserImage = async => {
-  const url = 'https://api.pinterest.com/v1/first_name?access_token={token}'
-  const response = await fetch(url);
+  const url = 'https://api.pinterest.com/v1/oauth/token';
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
 }
+
+
 
