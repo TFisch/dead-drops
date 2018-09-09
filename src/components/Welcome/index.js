44518c17fa257f4fc1410161b1cb84df7e6ae28e
Tim Fischer <emailtimfischer@gmail.com>
2018-09-08 22:36:15 -0600

    Change uri in Welcome

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -10,14 +10,13 @@ export const Welcome = () => {
     <div className="welcome">
       <h1>WELCOME</h1>
       <OauthSender
-        authorizeUrl="https://api.pinterest.com/oauth/"
+        authorizeUrl="http://api.pinterest.com/oauth/"
         clientId={'4987807426915878592'}
-        redirectUri="https://localhost:3000/"
+        redirectUri="https://99338ecb.ngrok.io/profile"
         // state={{ from: '/settings' }}
         args={{ scope: 'read_public', response_type: 'code' }}
         render={({ url }) => <a href={url}>LINK</a>}
       />
-
     </div>
 
   )
