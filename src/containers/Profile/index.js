952f841ffa6f7034d1eaece299ceace5a2d07e02
Tim Fischer <emailtimfischer@gmail.com>
2018-09-09 13:29:54 -0600

    Add imports to profile

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import HandleUser from '../../components/HandleUser';
-import { retrieveToken } from '../../api/apiCalls'
+import { retrieveToken } from '../../api/apiCalls';
+import { getProfileImage } from '../../actions/index';
+import { connect } from 'react-redux';
 
 
 export class Profile extends Component {
