87558f96b8eb0200b422b6c26ff1f222ffa107ea
Tim Fischer <emailtimfischer@gmail.com>
2018-09-10 16:48:18 -0600

    Fetch authorization

--- src/components/HandleUser/index.js
+++ src/components/HandleUser/index.js
@@ -6,17 +6,31 @@ export default class HandleUser extends Component {
   constructor() {
     super()
     this.state = {
-      accessCode: ''
+      authorizationCode: ''
     }
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     let code;
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (value) {
       code = value;
-      console.log(code);
     });
-    this.setState({ accessCode: code })
+    const newCode = code.split('').splice(6, code.length).join('')
+    await this.setState({ authorizationCode: newCode })
+    await this.getAccessToken()
+  }
+
+  async getAccessToken() {
+    const accessTokenUrl = 'https://api.pinterest.com/v1/oauth/token?' +
+      'grant_type=authorization_code&' +
+      'client_id=4987807426915878592&' +
+      `client_secret=${id}&` +
+      `code=${this.state.authorizationCode}`
+    const response = await fetch(accessTokenUrl, {
+      method: 'POST',
+    })
+    const result = await response.json()
+    debugger
   }
 
   handleSuccess = async (accessToken, { response, state }) => {
@@ -34,28 +48,32 @@ export default class HandleUser extends Component {
   render() {
     const url = 'https://api.pinterest.com/v1/oauth/token'
     return (
-      <OauthReceiver
-        tokenUrl={url}
-        clientId={'4987807426915878592'}
-        clientSecret={id}
-        redirectUri="https://6341b039.ngrok.io/profile"
-        onAuthSuccess={this.handleSuccess}
-        onAuthError={this.handleError}
-        args={{ grant_type: 'authorization_code', }}
-        tokenFetchArgs={{
-          method: 'POST', headers: {
-            'Content-Type': 'application/json',
-          }
-        }}
-        render={({ processing, state, error }) => (
-          <div>
-            {processing && <p>Authorizing now...</p>}
-            {error && (
-              <p className="error">An error occured: {error.message}</p>
-            )}
-          </div>
-        )}
-      />
+      // <OauthReceiver
+      //   tokenUrl={url}
+      //   clientId={'4987807426915878592'}
+      //   clientSecret={id}
+      //   redirectUri="https://6341b039.ngrok.io/profile"
+      //   onAuthSuccess={this.handleSuccess}
+      //   onAuthError={this.handleError}
+      //   args={{
+      //     grant_type: 'authorization_code',
+      //     code: this.state.accessCode,
+      //     client_id: '4987807426915878592',
+      //     client_secret: id
+      //   }}
+
+      //   render={({ processing, state, error }) => (
+      //     <div>
+      //       {processing && <p>Authorizing now...</p>}
+      //       {error && (
+      //         <p className="error">An error occured: {error.message}</p>
+      //       )}
+      //     </div>
+      //   )}
+      // />
+      <div>
+
+      </div>
     );
   }
 }
--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -6,17 +6,23 @@ import HandleUser from '../HandleUser';
 import { id } from '../../hidden/hidden'
 
 export const Welcome = () => {
+  const url = "https://api.pinterest.com/oauth/?" +
+    "response_type=code&" +
+    "redirect_uri=https://6341b039.ngrok.io/profile&" +
+    "scope=read_public,write_public,read_relationships,write_relationships&" +
+    "client_id=4987807426915878592"
   return (
     <div className="welcome">
       <h1>WELCOME</h1>
-      <OauthSender
+      {/* <OauthSender
         authorizeUrl="https://api.pinterest.com/oauth/"
         clientId={'4987807426915878592'}
         redirectUri="https://6341b039.ngrok.io/profile"
         // state={{ from: '/settings' }}
         args={{ scope: 'read_public', response_type: 'code' }}
         render={({ url }) => <a href={url}>Sign In With Pinterest</a>}
-      />
+      /> */}
+      <a href={url}>Start the process</a>
     </div>
 
   )
