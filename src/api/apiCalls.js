5dc295b2ca36d2333425beae3f58d950ef3d8a1c
Tim Fischer <emailtimfischer@gmail.com>
2018-09-11 12:38:36 -0600

    Import clean image helper to fetch

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,9 +1,11 @@
+import { cleanImage } from '../utilities/helper'
+
 
 export const fetchUserImage = async (token) => {
   const url = `https://api.pinterest.com/v1/users/me/?access_token=${token}&fields=image`
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data)
+
 }
 
 
