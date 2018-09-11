3888cc37544f456aed0abc0e6738a54a114461b4
Tim Fischer <emailtimfischer@gmail.com>
2018-09-10 19:32:24 -0600

    Remove unused sender

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 export const getProfileImage = (userInfo) => ({
   type: 'GET_IMAGE',
   userInfo
-})
+});
 
 export const getToken = (token) => ({
   type: 'GET_TOKEN',
   token
-})
+});
--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './style.css';
 import { Route } from 'react-router-dom';
 import { OauthSender, OauthReceiver } from 'react-oauth-flow';
-import HandleUser from '../HandleUser';
 import { id } from '../../hidden/hidden'
 
 export const Welcome = () => {
@@ -14,14 +13,6 @@ export const Welcome = () => {
   return (
     <div className="welcome">
       <h1>WELCOME</h1>
-      {/* <OauthSender
-        authorizeUrl="https://api.pinterest.com/oauth/"
-        clientId={'4987807426915878592'}
-        redirectUri="https://6341b039.ngrok.io/profile"
-        // state={{ from: '/settings' }}
-        args={{ scope: 'read_public', response_type: 'code' }}
-        render={({ url }) => <a href={url}>Sign In With Pinterest</a>}
-      /> */}
       <a href={url}>Start the process</a>
     </div>
 
