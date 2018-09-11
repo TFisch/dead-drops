8315e4c1078aff75987c77edbe69f07f3b4366c9
Tim Fischer <emailtimfischer@gmail.com>
2018-09-11 08:15:42 -0600

    combine actions in userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,9 @@
-export const userReducer = (state = [], action) => {
+export const userReducer = (state = {}, action) => {
   switch (action.type) {
     case 'GET_IMAGE':
-      return action.image; 
+      return { ...state, image: action.image };
+    case 'GET_TOKEN':
+      return { ...state, token: action.token };
     default:
       return state;
   }
