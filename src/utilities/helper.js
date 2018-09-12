f77813b4e5d14542d9c8334b0ca83248498e4edd
Tim Fischer <emailtimfischer@gmail.com>
2018-09-11 22:31:44 -0600

    store Username

--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -1,4 +1,7 @@
 export const cleanImage = (imageResponse) => {
   const image = imageResponse.data.image;
+  const username = imageResponse.data.username
+  console.log(username);
   return image['60x60'].url;
 }
+
