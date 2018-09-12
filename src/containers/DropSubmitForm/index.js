dbb45f5b3899b5eb6166f8e297e34fa8626e7f9c
Tim Fischer <emailtimfischer@gmail.com>
2018-09-12 00:12:43 -0600

    Change difficulty to drop down

--- src/containers/DropSubmitForm/index.js
+++ src/containers/DropSubmitForm/index.js
@@ -8,7 +8,11 @@ export const DropSubmitForm = () => {
         <div className="drop-form">
           <span className="entry-row">
             <h3>Difficulty</h3>
-            <input name="difficulty" placeholder="difficulty" type="text" />
+            <select name="difficulty">
+              <option value="easy">Easy</option>
+              <option value="intermediate">Intermediate</option>
+              <option value="Hard">Hard</option>
+            </select>
           </span>
           <span className="entry-row">
             <h3>Northern Coordinates</h3>
@@ -21,12 +25,13 @@ export const DropSubmitForm = () => {
           <span className="entry-row">
             <h3>Unique Verification Code</h3>
             <div className="unique-code-entry-wrap">
-              <input className="code-entry" maxlength="1" type="text" />
-              <input className="code-entry" maxlength="1" type="text" />
-              <input className="code-entry" maxlength="1" type="text" />
-              <input className="code-entry" maxlength="1" type="text" />
-              <input className="code-entry" maxlength="1" type="text" />
-              <input className="code-entry" maxlength="1" type="text" />
+              <input className="code-entry" name="unique-one" maxlength="1" type="text" />
+              <input className="code-entry" name="unique-two" maxlength="1" type="text" />
+              <input className="code-entry" name="unique-three" maxlength="1" type="text" />
+              <input className="code-entry" name="unique-four" maxlength="1" type="text" />
+              <input className="code-entry" name="unique-five" maxlength="1" type="text" />
+              <input className="code-entry" name="unique-six" maxlength="1" type="text" />
+
             </div>
           </span>
           <span className="button-row">
