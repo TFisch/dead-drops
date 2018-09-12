62f4cad49e59317cc0726df3f1547927e0016760
Tim Fischer <emailtimfischer@gmail.com>
2018-09-11 22:58:02 -0600

    Remove drop submit from nav

--- src/containers/Nav/index.js
+++ src/containers/Nav/index.js
@@ -5,7 +5,6 @@ import './style.css';
 export const Nav = () => {
   return (
     <div className="nav">
-      <Link to='/dropSubmitForm'><button className="post-button">Post A Drop</button></Link>
       <Link to='/'><button className="logout-button">Log Out</button></Link>
     </div>
   )
