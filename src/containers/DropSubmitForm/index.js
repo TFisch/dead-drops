ff4083fc7e7fe262bc680d81c75ec882de70e256
Tim Fischer <emailtimfischer@gmail.com>
2018-09-11 23:21:26 -0600

    add drop form container

--- src/containers/DropSubmitForm/index.js
+++ src/containers/DropSubmitForm/index.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './DropSubmitForm.css'
 
 export const DropSubmitForm = () => {
   return (
     <div>
       <div className="drop-submit">
-        <div className="form">
+        <div className="drop-form">
           <input name="difficulty" placeholder="difficulty" type="text" />
           <input name="north-coord" placeholder="north coordinates" type="text" />
           <input name="west-coord" placeholder="west coordinates" type="text" />
-          <input name="code" placeholder="Unique Verification Code" type="text" />
-          <button>SUBMIT</button>
+          <input name="code-entry" maxlength="1" placeholder="Unique Verification Code" type="text" />
+          <button className="submit-button form-row">SUBMIT</button>
         </div>
       </div>
     </div>
