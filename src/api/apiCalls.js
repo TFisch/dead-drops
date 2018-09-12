693bc613a298edfbfd07533a1efbbf25881daae2
Tim Fischer <emailtimfischer@gmail.com>
2018-09-11 22:30:44 -0600

    Change dropForm to component

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,13 +1,14 @@
-import { cleanImage } from '../utilities/helper'
+import { cleanImage, cleanUsername } from '../utilities/helper'
 
 
 export const fetchUserImage = async (token) => {
-  const url = `https://api.pinterest.com/v1/users/me/?access_token=${token}&fields=image`
+  const url = `https://api.pinterest.com/v1/users/me/?access_token=${token}&fields=image%2Cusername`
   const response = await fetch(url);
   const data = await response.json();
   const cleanedImage = await cleanImage(data);
   return cleanedImage;
 }
+
 
 
 
