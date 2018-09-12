66356f1e1eb614a90cac60d628199e15f8b5ce40
Tim Fischer <emailtimfischer@gmail.com>
2018-09-11 21:24:00 -0600

    Await cleaned Image in fetchUserImage

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -5,7 +5,8 @@ export const fetchUserImage = async (token) => {
   const url = `https://api.pinterest.com/v1/users/me/?access_token=${token}&fields=image`
   const response = await fetch(url);
   const data = await response.json();
-
+  const cleanedImage = await cleanImage(data);
+  return cleanedImage;
 }
 
 
