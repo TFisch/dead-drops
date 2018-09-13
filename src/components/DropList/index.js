2b88c8c968d38c16a909c0e4604a19360a3cf2f3
Tim Fischer <emailtimfischer@gmail.com>
2018-09-12 19:21:40 -0600

    Pass image to Card from List via props

--- src/components/DropList/index.js
+++ src/components/DropList/index.js
@@ -2,12 +2,10 @@ import React from 'react'
 import DropCard from '../DropCard'
 
 export const DropList = (props) => {
-  const displayPins = props.retrievedBoard.map(pin => <DropCard note={pin.note} url={pin.url} />);
+  const displayPins = props.retrievedBoard.map(pin => <DropCard note={pin.note} image={pin.image.original.url} />);
   return (
     <div className='drop-list'>
       {displayPins}
     </div>
   )
 }
-
-
