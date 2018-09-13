ce272500a4be392a85603e69e65cb3a06f4ce8ed
Tim Fischer <emailtimfischer@gmail.com>
2018-09-12 23:18:49 -0600

    Write mockData

--- src/utilities/mockData/index.js
+++ src/utilities/mockData/index.js
@@ -1,2 +1,7 @@
-export const tokenReducerMock = 'AotyU8WH1d1jf6hbf_a5y4HeZ-StFVPaN5kRpsdFOEBtoWBGwAn_ADAAAabaRTpF9OxgaLYAAAAA'
+export const tokenMock = 'AotyU8WH1d1jf6hbf_a5y4HeZ-StFVPaN5kRpsdFOEBtoWBGwAn_ADAAAabaRTpF9OxgaLYAAAAA'
 
+export const userReducerMock = { username: 'clark', image: 'https://via.placeholder.com/350x150' }
+
+export const mockUsername = "Harvey";
+
+export const mockImage = 'https://via.placeholder.com/350x150'; 
