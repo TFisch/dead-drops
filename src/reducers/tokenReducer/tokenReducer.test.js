edc03fb0737e28f57e2cb3094d397c6a009fe1aa
Tim Fischer <emailtimfischer@gmail.com>
2018-09-12 22:33:57 -0600

    Write tokenReducer retrieve test

---
+++ src/reducers/tokenReducer/tokenReducer.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { tokenReducer } from '../tokenReducer'
+import { tokenReducerMock } from '../../utilities/mockData'
+import * as actions from '../../actions/';
+
+
+describe('tokenReducer', () => {
+  const { getToken } = actions;
+
+  it('should return the initial State', () => {
+    const expected = {};
+
+    const result = tokenReducer(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should retieve the token', () => {
+
+    expect(tokenReducer(undefined, getToken(tokenReducerMock))).toEqual(tokenReducerMock)
+  });
+
+});
