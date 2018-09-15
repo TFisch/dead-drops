e9b3e54b33ea3499d0d06493ceec9d59a0202eac
Tim Fischer <emailtimfischer@gmail.com>
2018-09-15 14:39:51 -0600

    Put location in rootReducer

--- src/reducers/rootReducer/index.js
+++ src/reducers/rootReducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { tokenReducer } from '../tokenReducer'
 import { userReducer } from '../userReducer'
+import { locationReducer } from '../locationReducer'
 
 export const rootReducer = combineReducers({
   token: tokenReducer,
-  user: userReducer
+  user: userReducer,
+  location: locationReducer
 })
