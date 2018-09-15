5f714f57b6ecc1b1893aa4632fbe5c7b386f8246
Tim Fischer <emailtimfischer@gmail.com>
2018-09-15 14:38:52 -0600

    map display pins to list

--- src/components/DropCard/index.js
+++ src/components/DropCard/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import './DropCard.css'
 
 export default (props) => {
-  const { note, image } = props
+  const { note } = props
+  const image = props.image.original.url;
   return (
     <div className="drop-card">
       <img src={image} alt="drop-map-location" className="drop-image" />
--- src/components/DropList/index.js
+++ src/components/DropList/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import DropCard from '../DropCard'
 
 export const DropList = (props) => {
-  const displayPins = props.retrievedBoard.map(pin => <DropCard note={pin.note} />);
+  const displayPins = props.retrievedBoard.map(pin => <DropCard {...pin} />);
   return (
     <div className='drop-list'>
       {displayPins}
