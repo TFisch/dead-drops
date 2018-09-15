9483a04006bccdc3c34f4986ff1da30ae9812bf6
Tim Fischer <emailtimfischer@gmail.com>
2018-09-15 14:37:14 -0600

    Create getLocation action

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,3 +8,10 @@ export const getToken = (token) => ({
   type: 'GET_TOKEN',
   token
 });
+
+export const getLocation = ({ long, lat, image }) => ({
+  type: 'GET_LOCATION',
+  long,
+  lat,
+  image
+})
