ba67bd1c5158cad44de99cabc6eadb94eeea98ad
Tim Fischer <emailtimfischer@gmail.com>
2018-09-16 11:29:17 -0600

    Create mockLocation and mockLocationReducer mockdata

--- src/utilities/mockData/index.js
+++ src/utilities/mockData/index.js
@@ -6,4 +6,18 @@ export const mockUsername = "Harvey";
 
 export const mockImage = 'https://via.placeholder.com/350x150';
 
-export const mockUser = { username: mockUsername, image: mockImage }
+export const mockUser = { username: mockUsername, image: mockImage };
+
+export const mockLocation = {
+  long: '3214321504',
+  lat: '2.52343245',
+  locationImage: 'https://via.placeholder.com/350x150',
+  verificationCode: [0, 0, 0, 0, 0, 0]
+};
+
+export const mockLocationReducer = {
+  longitude: '3214321504',
+  latitude: '2.52343245',
+  image: 'https://via.placeholder.com/350x150',
+  verificationCode: [0, 0, 0, 0, 0, 0]
+};
