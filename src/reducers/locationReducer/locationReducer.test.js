db13b87d4e3e986a4a3c5c065960a3e8c6619e38
Tim Fischer <emailtimfischer@gmail.com>
2018-09-16 11:28:45 -0600

    Test location reducer

---
+++ src/reducers/locationReducer/locationReducer.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { locationReducer } from '../locationReducer'
+import { mockLocation, mockLocationReducer } from '../../utilities/mockData'
+import * as actions from '../../actions/';
+
+
+describe('locationReducer', () => {
+  const { getLocation } = actions;
+
+  it('should return the initial State', () => {
+    const expected = {};
+
+    const result = locationReducer(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should retieve the token', () => {
+
+    expect(locationReducer(undefined, getLocation(mockLocation))).toEqual(mockLocationReducer)
+  });
+
+});
