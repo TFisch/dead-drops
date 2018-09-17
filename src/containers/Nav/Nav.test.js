2e6a8097579455f6c5a14f97a30523226f2aa35f
Tim Fischer <emailtimfischer@gmail.com>
2018-09-17 11:13:38 -0600

    Write tests for handleSubmitButton, handleProfileButton

--- src/containers/Nav/Nav.test.js
+++ src/containers/Nav/Nav.test.js
@@ -1,17 +1,56 @@
 import React from 'react';
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import { Nav } from './'
-import { mockUser } from '../../utilities/mockData'
+import { mockUser, mockPropsMethod } from '../../utilities/mockData'
+import { setFormActive, resetConfirm, resetFormActive } from '../../actions'
+import { handleSubmitButton } from './';
 
 describe('Nav', () => {
   let wrapper;
+  const mockPropsMethodTwo = mockPropsMethod;
+  const mockPropsMethodThree = mockPropsMethod;
 
   beforeEach(() => {
-    wrapper = shallow(<Nav user={mockUser} />)
+    wrapper = shallow(
+      <Nav
+        user={mockUser}
+        setFormActive={mockPropsMethod}
+        resetConfirm={mockPropsMethodTwo}
+        resetFormActive={mockPropsMethodThree}
+      />
+    )
   });
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('handleSubmit', () => {
+
+    it('should call setFormActive when called', () => {
+      wrapper.instance().handleSubmitButton();
+
+      expect(mockPropsMethod).toHaveBeenCalled();
+    });
+
+  });
+
+  describe('handleProfileButton', () => {
+    it('should call resetConfirm when called', () => {
+
+      wrapper.instance().handleProfileButton();
+
+      expect(mockPropsMethodTwo).toHaveBeenCalled();
+
+    })
+
+    it('should call resetFormActive when called', () => {
+
+      wrapper.instance().handleProfileButton();
+
+      expect(mockPropsMethodThree).toHaveBeenCalled();
+
+    })
   });
 
 });
