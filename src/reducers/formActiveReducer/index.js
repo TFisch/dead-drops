5bc7e95fba07d6bcf2ec05d5a8009cee9478fccd
Tim Fischer <emailtimfischer@gmail.com>
2018-09-16 19:26:29 -0600

    create formActiveReducer

---
+++ src/reducers/formActiveReducer/index.js
@@ -0,0 +1,12 @@
+export const formActiveReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'SET_FORM_ACTIVE':
+      return action.setForm;
+    case 'RESET_FORM_ACTIVE':
+      return {};
+    default:
+      return state;
+  }
+}
+
+
