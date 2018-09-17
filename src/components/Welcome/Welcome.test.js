ece90d79e97d2e188215fc204c02095d7e263292
Tim Fischer <emailtimfischer@gmail.com>
2018-09-17 13:18:42 -0600

    Write Welcome snapshot test

---
+++ src/components/Welcome/Welcome.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Welcome } from './'
+import { shallow } from 'enzyme'
+
+describe('Welcome', () => {
+  let wrapper;
+
+  beforeEach(() => {
+
+    wrapper = shallow(<Welcome
+    />)
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
