fe704f49ef98fdd10123294e6b0192a2ada115e5
Tim Fischer <emailtimfischer@gmail.com>
2018-09-17 21:38:48 -0600

    Test confirmReducer

---
+++ src/reducers/confirmReducer/confirmReducer.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { confirmReducer } from './'
+import * as actions from '../../actions/';
+
+
+describe('confirmReducer', () => {
+  const { setConfirm, resetConfirm } = actions;
+  const mockStatus = true;
+
+  it('should return the initial State', () => {
+    const expected = {};
+
+    const result = confirmReducer(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should set the confirm state to true', () => {
+    const mockConfirmReducer = true;
+
+    expect(confirmReducer(undefined, setConfirm(mockStatus))).toEqual(mockConfirmReducer)
+  });
+
+  it('should clear the confirm state to an empty object', () => {
+    const mockConfirmReducer = {};
+
+    expect(confirmReducer(undefined, resetConfirm())).toEqual(mockConfirmReducer)
+  });
+
+});
