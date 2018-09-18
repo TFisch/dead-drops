d99ef45a318a07fcddd8e85ebab9cd3ee98e6249
Tim Fischer <emailtimfischer@gmail.com>
2018-09-17 19:41:46 -0600

    Test handleChange

--- src/containers/DropSubmitForm/DropSubmitForm.test.js
+++ src/containers/DropSubmitForm/DropSubmitForm.test.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { DropSubmitForm } from './'
 import { shallow } from 'enzyme'
+import { mockPropsMethod } from '../../utilities/mockData'
 
 describe('DropSubmitForm', () => {
   let wrapper;
 
   beforeEach(() => {
 
-    wrapper = shallow(<DropSubmitForm
+    wrapper = shallow(<DropSubmitForm handleChange={mockPropsMethod}
     />)
   });
 
@@ -15,4 +16,39 @@ describe('DropSubmitForm', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-});
+  describe('handleChange', () => {
+    it('should be called when an on change occurs', () => {
+
+      const event = { target: { value: '213' } };
+
+      const spy = jest.spyOn(wrapper.instance(), 'handleChange');
+      wrapper.instance().forceUpdate();
+
+      wrapper.find('[name="longitude"]').simulate('change', event);
+
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+  })
+
+
+  describe('handleChange', () => {
+    it('should be called when an on change occurs', () => {
+
+      const event = { target: [0] };
+
+      const spy = jest.spyOn(wrapper.instance(), 'handleCode');
+      wrapper.instance().forceUpdate();
+
+      wrapper.find('[name="uniqueOne"]').simulate('change', event);
+
+
+      expect(spy).toHaveBeenCalled();
+    });
+  })
+
+})
+
+
+
