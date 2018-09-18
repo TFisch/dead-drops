36089fb736e6467ae565b3ceb81e2d0fa9851a52
Tim Fischer <emailtimfischer@gmail.com>
2018-09-17 21:38:38 -0600

    Test formActiveReducer

---
+++ src/reducers/formActiveReducer/formActiveReducer.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { formActiveReducer } from './'
+import * as actions from '../../actions/';
+
+
+describe('formActiveReducer', () => {
+  const { setFormActive, resetFormActive } = actions;
+  const mockStatus = true;
+
+  it('should return the initial State', () => {
+    const expected = {};
+
+    const result = formActiveReducer(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should set the formActive state to true', () => {
+    const mockFormActiveReducer = true;
+
+    expect(formActiveReducer(undefined, setFormActive(mockStatus))).toEqual(mockFormActiveReducer)
+  });
+
+  it('should clear the confirm state to an empty object', () => {
+    const mockFormActiveReducer = {};
+
+    expect(formActiveReducer(undefined, resetFormActive())).toEqual(mockFormActiveReducer)
+  });
+
+});
