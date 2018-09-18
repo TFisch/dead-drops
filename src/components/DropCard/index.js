ea49c900851cbd8dbf269ea94fc2891191bd9f33
Tim Fischer <emailtimfischer@gmail.com>
2018-09-17 19:42:33 -0600

    Add drop icon

--- src/components/DropCard/index.js
+++ src/components/DropCard/index.js
@@ -5,6 +5,7 @@ export const DropCard = (props) => {
   const { note } = props
   return (
     <div className="drop-card">
+      <img className="drop-icon" src={require('../../utilities/images/treasure.svg')} alt="treasure-icon" />
       <p>{note}</p>
     </div>
   )
